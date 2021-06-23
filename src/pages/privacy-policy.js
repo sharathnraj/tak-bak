3088e48c4bc2ffe4bb54dd3bd69d8f0dce6b02a9
Sharath <sharath.nagaraj@cheproximity.com.au>
2021-06-23 10:18:20 +1000

    Copy

--- src/pages/privacy-policy.js
+++ src/pages/privacy-policy.js
@@ -14,6 +14,7 @@ const PrivacyPolicy = () => (
           Thank you for choosing to be part of our community at Shwetha Nagaraj (“Company”, “we”, “us”, or “our”). We are committed to protecting your personal information and your right to privacy. If you have any questions or concerns about our notice, or our practices with regards to your personal information, please contact us at <a href="mailto:shwethan@outlook.com">shwethan@outlook.com</a>.<br /><br />
           When you visit our mobile application, and use our services, you trust us with your personal information. We take your privacy very seriously. In this privacy notice, we seek to explain to you in the clearest way possible what information we collect, how we use it and what rights you have in relation to it. We hope you take some time to read through it carefully, as it is important. If there are any terms in this privacy notice that you do not agree with, please discontinue use of our Apps and our services.<br /><br />
           This privacy notice applies to all information collected through our mobile application, (\"Apps\"), and/or any related services, sales, marketing or events (we refer to them collectively in this privacy notice as the \"Services\").<br /><br />
+          We also keep track of the scores using leaderboards and the scores are pushed to a common server.<br /><br />
           Please read this privacy notice carefully as it will help you make informed decisions about sharing your personal information with us.
           <br />
           <br />
